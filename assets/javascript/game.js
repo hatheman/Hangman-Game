1ead7a71bc7616e42466bc6ac86f1d091640e66f
Min Soo Ha <minsooha@mins-mbp.home>
2018-02-17 08:33:00 -0800

    <final progress>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,13 @@
 
-//first create an array of correct answers. 
-
-//     var questions= {
-//     q1: ["honda"],
-//     q2: ["hyundai"],
-//     q3: ["lexus"],
-//     q4: ["fiat"],
-//   }
-
+//answers and variables
 var answers= ["honda", "hyundai", "lexus", "fiat"]
 
 var alphabet= ("abcdefghijklmnopqrstuvwxyz").split("");
 
 
 console.log(alphabet)
-//     q1: ["honda"],
-//     q2: ["hyundai"],
-//     q3: ["lexus"],
-//     q4: ["fiat"]
-// }
+
+//randomly input word for the question
 var answer = answers[Math.floor(Math.random() * answers.length)];
 
 
@@ -31,7 +20,7 @@ var questionIndex = 0;
 
 var guesses = document.querySelector("#guesses-left").innerHTML
 
-//get underscore to represent word
+//create underscore for answer
 var answerArray = [];
 for (var i=0; i<answer.length; i++) {
     answerArray[i] = "_";
@@ -41,47 +30,27 @@ for (var i=0; i<answer.length; i++) {
 
 var remainingLetters = answer.length;
 
-// function renderQuestion() {
-//     if (questionIndex < 9) {
-//         document.querySelector("#word-blanks").innerHTML = answerArray;
-//     }
-//      else {
-//          alert("Game Over!")
-//         document.querySelector("#win-score").innerHTML = "Final Score: " + score
-//      }
-// }
-
-
-
-
-// for (var j=0; j<answer.length; j++) {
-    // System.out.print(answerArray[j]);
-// }
 
 
 
 
 
-        
 
-//     }
-//      else {
-//         document.querySelector("#win-counter").innerHTML = "Final Score: " + score;
-//      }
-//  }
+//function to increase winning score
 function winScore(){
     document.querySelector("#win-counter").innerHTML = incScore
     if( incScore < 0) {
         incScore = 0
     }
         } 
-
+//function to increase losing score
 function loseScore() {
     document.querySelector("#loss-counter").innerHTML = decScore
     if(decScore < 0 ) {
         decScore = 0
     }
 }
+//function to decrease guesses left
 function updateGuesses() {
     document.querySelector("#guesses-left").innerHTML = guesses 
     if(guesses < 0) {
@@ -89,23 +58,16 @@ function updateGuesses() {
     }
     }
 
+    
 
-// updateScore();
-
-// var wordBlanks = $("#word-blanks"); 
-
-
-//renderQuestion()
 
 
 document.onkeyup = function(event) {
-    //  if (questionIndex === answer.length) {
-        // return;
-    // }
+    
     var userInput = String.fromCharCode(event.which).toLowerCase();
 
 
-// //for (var i=0; i<correctAnswers.length; i++) {
+
   if (remainingLetters === 0) {
     alert("i won")
     incScore++ 
@@ -120,7 +82,8 @@ document.onkeyup = function(event) {
         loseScore()
         return;
     }
-//input characters to underscore
+
+
     if (alphabet.indexOf(userInput) > -1) {
         guesses--
         updateGuesses()
@@ -143,15 +106,10 @@ document.onkeyup = function(event) {
                 
             }}
            
-        // if (userInput === answer[i]) {
-            // var str= document.getElementById("word-blanks").innerHTML
-           // var res = str.replace(answerArray[i], userInput)
-            // document.getElementById("word-blanks").innerHTML = res
+        
         
             
             
-            // updateScore()
-           console.log("it is correct")
 
             }
         
@@ -168,12 +126,7 @@ document.onkeyup = function(event) {
     }
     function myFunction() {
         document.getElementById("myForm").reset();}
-    //renderQuestion()
-    
-
-
-        // questionIndex++;
-        //renderQuestion();
+   
     
         
 
@@ -184,9 +137,6 @@ document.onkeyup = function(event) {
 
 
 
-    //if ((userText == "h") || (userText == "o") || (userText == "n") || (userText == "d") || (userText == "a")) {
-       // var answer = str.replace("_", 'h,o,n,d,a' )
-       // document.write(answer)
-
+   
 
     
